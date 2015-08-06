9228c1b5afeb7930e15d0a4fab486051f0961ec6
HuzefaGadi <learndroid53@gmail.com>
2015-08-06 17:20:52 +0530

    Update SettingsActivity.java

--- app/src/main/java/com/lazydroid/incaseof/SettingsActivity.java
+++ app/src/main/java/com/lazydroid/incaseof/SettingsActivity.java
@@ -1,17 +1,118 @@
 package com.lazydroid.incaseof;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 
 public class SettingsActivity extends ActionBarActivity {
+
+    Button addressBook, save;
+    EditText emailAddresses, interval;
+    Spinner exposure;
+    RadioGroup radioGroup;
+    SharedPreferences preferences;
+    SharedPreferences.Editor edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        radioGroup = (RadioGroup) findViewById(R.id.camera_settings);
+        exposure = (Spinner) findViewById(R.id.exposureText);
+        interval = (EditText) findViewById(R.id.shotIntervalText);
+        save = (Button) findViewById(R.id.save);
+        addressBook = (Button) findViewById(R.id.address_book);
+        emailAddresses = (EditText) findViewById(R.id.emailAddress);
+        preferences = getSharedPreferences(InCaseOfApp.PREFERENCES, MODE_PRIVATE);
+        edit = preferences.edit();
+        emailAddresses.setText(preferences.getString(InCaseOfApp.SHOOTING_EMAIL_ADDRESS, ""));
+        interval.setText(preferences.getInt(InCaseOfApp.SHOOTING_INTERVAL, 0) + "");
+        exposure.setSelection(preferences.getInt(InCaseOfApp.SHOOTING_EXPOSURE, 0));
+        int flash = preferences.getInt(InCaseOfApp.SHOOTING_FLASH, 0);
+        switch (flash) {
+            case 1:
+
+                radioGroup.check(R.id.on);
+                break;
+            case 2:
+                radioGroup.check(R.id.off);
+                break;
+            case 0:
+                radioGroup.check(R.id.auto);
+                break;
+            default:
+                radioGroup.check(R.id.auto);
+                break;
+        }
+        addressBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(ContactsContract.CommonDataKinds.Email.CONTENT_TYPE);
+                startActivityForResult(intent, 1);
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int flash = 0;
+                switch (radioGroup.getCheckedRadioButtonId()) {
+                    case R.id.on:
+                        flash = 1;
+                        break;
+                    case R.id.off:
+                        flash = 2;
+                        break;
+                    case R.id.auto:
+                        flash = 0;
+                        break;
+                    default:
+                        flash = 0;
+                        break;
+                }
+                edit.putInt(InCaseOfApp.SHOOTING_INTERVAL, Integer.parseInt(interval.getText().toString()));
+                edit.putInt(InCaseOfApp.SHOOTING_FLASH, flash);
+                edit.putInt(InCaseOfApp.SHOOTING_EXPOSURE,exposure.getSelectedItemPosition());
+                edit.putString(InCaseOfApp.SHOOTING_EMAIL_ADDRESS, emailAddresses.getText().toString().trim());
+                edit.commit();
+
+                Toast.makeText(getApplicationContext(), "Saved successfully", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (resultCode == Activity.RESULT_OK) {
+                Uri contactData = data.getData();
+                Cursor cursor = this.managedQuery(contactData, null, null, null, null);
+                cursor.moveToFirst();
+                String email = cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                emailAddresses.setText(emailAddresses.getText() + email + ",");
+            }
+
+
+        }
     }
 
     @Override
