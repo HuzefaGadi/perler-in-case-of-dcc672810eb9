2b571efd6b2ab92a932318afe3e64b1d2f907488
HuzefaGadi <learndroid53@gmail.com>
2015-08-06 17:20:29 +0530

    Update InCaseOfApp.java

--- app/src/main/java/com/lazydroid/incaseof/InCaseOfApp.java
+++ app/src/main/java/com/lazydroid/incaseof/InCaseOfApp.java
@@ -6,27 +6,30 @@ import android.content.SharedPreferences;
 import android.provider.Settings.Secure;
 
 public class InCaseOfApp extends Application {
-	public static final boolean DEBUG = false;	// enables debug output in logs
+    public static final boolean DEBUG = false;    // enables debug output in logs
 
-	private static InCaseOfApp instance;
+    private static InCaseOfApp instance;
 
-	public static SharedPreferences preferences;
+    public static String SHOOTING_EMAIL_ADDRESS = "email_addressess";
+    public static String SHOOTING_INTERVAL = "interval";
+    public static String SHOOTING_FLASH = "flash";
+    public static String SHOOTING_EXPOSURE = "exposure";
+    public static String PREFERENCES = "preferences";
 
-	public static String android_id;
+    public static String android_id;
 
-	// used to prevent erroneous camera restarts
-	public static long camera_done = 0;
+    // used to prevent erroneous camera restarts
+    public static long camera_done = 0;
 
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		instance = this;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
 
-		preferences = getSharedPreferences( getPackageName() + "_preferences", MODE_PRIVATE);
-		android_id = Secure.getString( this.getContentResolver(), Secure.ANDROID_ID);
-	}
+        android_id = Secure.getString(this.getContentResolver(), Secure.ANDROID_ID);
+    }
 
-	public static Context getContext() {
-		return instance;
-	}
+    public static Context getContext() {
+        return instance;
+    }
 }
